344a27c5c833236e7efa60be0571111ba81bd41a
Regiss Mukubiza <regiss.steven@gmail.com>
2022-04-20 13:56:45 +0200

    pure components

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export class Calculator extends PureComponent {
+  render() {
+    return (
+      <div>Calculator</div>
+    );
+  }
+}
+
+export default Calculator;
