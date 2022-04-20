2ee2d7250ba1a5acf3d32b3ea86abb6889c2c2a7
Regiss Mukubiza <regiss.steven@gmail.com>
2022-04-20 14:25:37 +0200

    design complete

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,7 +3,46 @@ import React, { PureComponent } from 'react';
 export class Calculator extends PureComponent {
   render() {
     return (
-      <div>Calculator</div>
+      <>
+        <div className="calculator">
+          <div className="calculator__display">
+            <div className="calculator__display-value">0</div>
+          </div>
+          <div className="calculator__keypad">
+            <div className="calculator__keypad-row">
+              <div className="calculator__keypad-key">AC</div>
+              <div className="calculator__keypad-key">+/-</div>
+              <div className="calculator__keypad-key">%</div>
+              <div className="calculator__keypad-key sign">:</div>
+            </div>
+            <div className="calculator__keypad-row">
+              <div className="calculator__keypad-key">7</div>
+              <div className="calculator__keypad-key">8</div>
+              <div className="calculator__keypad-key">9</div>
+              <div className="calculator__keypad-key sign">x</div>
+            </div>
+            <div className="calculator__keypad-row">
+              <div className="calculator__keypad-key">4</div>
+              <div className="calculator__keypad-key">5</div>
+              <div className="calculator__keypad-key">6</div>
+              <div className="calculator__keypad-key sign">-</div>
+            </div>
+            <div className="calculator__keypad-row">
+              <div className="calculator__keypad-key">1</div>
+              <div className="calculator__keypad-key">2</div>
+              <div className="calculator__keypad-key">3</div>
+              <div className="calculator__keypad-key sign">+</div>
+            </div>
+            <div className="calculator__keypad-row">
+              <div className="calculator__keypad-key">0</div>
+              <div className="calculator__keypad-key">.</div>
+              <div className="calculator__keypad-key">=</div>
+              <div className="calculator__keypad-key sign">=</div>
+            </div>
+          </div>
+        </div>
+
+      </>
     );
   }
 }
